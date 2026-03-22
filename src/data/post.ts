0f298e67d8f4c65e002a6fa1ac0e56c1e3035716
kaueMarques <mbkaue@gmail.com>
2026-03-22 18:35:03 -0300

    page

--- src/data/post.ts
+++ src/data/post.ts
@@ -3,8 +3,7 @@ import { type CollectionEntry, getCollection } from "astro:content";
 /** filter out draft posts based on the environment */
 export async function getAllPosts(): Promise<CollectionEntry<"post">[]> {
 	return await getCollection("post", ({ data }) => {
-		const isNotEvent = !data.tags.includes("evento");
-		return import.meta.env.PROD ? !data.draft && isNotEvent : isNotEvent;
+		return import.meta.env.PROD ? !data.draft : true;
 	});
 }
 
